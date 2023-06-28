a4c311881c3b6b5d394a653b926d4cc11c8a25bb
Kyryl Boroda <kirilboroda25@gmail.com>
2023-06-28 23:45:39 +0300

    spring

--- src/main/java/webs/hillel/Cart.java
+++ src/main/java/webs/hillel/Cart.java
@@ -4,33 +4,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Component
 public class Cart {
     private List<Product> products;
-    @Autowired
+
     public Cart() {
         products = new ArrayList<>();
     }
 
+
     public void addProduct(Product product) {
-        products.add(product);
-    }
-    public boolean removeProduct(int id) {
-        Iterator<Product> iterator = products.iterator();
-        while (iterator.hasNext()) {
-            Product product = iterator.next();
-            if (product.getId() == id) {
-                iterator.remove();
-                return true;
-            }
+        if (product != null) {
+            products.add(product);
+        } else {
+            throw new IllegalArgumentException("Invalid product. Cannot add null to the cart.");
         }
+    }
+
+    public boolean removeProduct(int id) {
+        Optional<Product> productOptional = products.stream()
+                .filter(product -> product.getId() == id)
+                .findFirst();
+
+        if (productOptional.isPresent()) {
+            products.remove(productOptional.get());
+            return true;
+        }
+
         return false;
     }
+
 
     public List<Product> getProducts() {
         return products;
